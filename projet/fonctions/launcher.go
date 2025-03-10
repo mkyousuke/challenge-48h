458131e77696945299e75ff5312b88561263c848
madchip777 <51477281+madchip777@users.noreply.github.com>
2025-03-10 16:50:38 +0100

    Implementation un tester Worpress Page Tester

--- projet/fonctions/launcher.go
+++ projet/fonctions/launcher.go
@@ -7,4 +7,10 @@ import (
 // Lance l'application web
 func LaunchApp() {
 	fmt.Println("Lancement de l'application web")
+	err := IsWordPressSite("https://www.starwarsblog.net")
+	if err != nil {
+		fmt.Println("Erreur lors de la vérification du site WordPress: ", err)
+	} else {
+		fmt.Println("Le site EST un site WordPress")
+	}
 }
