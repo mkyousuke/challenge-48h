3931a4cb3ba1fd50d864c6910fc30650d1c0ba20
juju077176 <90097008+juju077176@users.noreply.github.com>
2025-03-11 11:27:50 +0100

    ajout

--- projet/fonctions/worldpressTester.go
+++ projet/fonctions/worldpressTester.go
@@ -3,6 +3,7 @@ package fonctions
 import (
 	"errors"
 	"net/http"
+	"encoding/json"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -41,3 +42,26 @@ func IsWordPressSite(url string) error {
 
 	return errors.New("le site n'est pas un site WordPress")
 }
+<<<<<<< Updated upstream
+=======
+
+func CheckWordPressHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	url := r.FormValue("site-url")
+	err := IsWordPressSite(url)
+	response := make(map[string]string)
+
+	if err != nil {
+		response["result"] = "Le site n'est pas un site WordPress"
+	} else {
+		response["result"] = "Le site est un site WordPress"
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+>>>>>>> Stashed changes
