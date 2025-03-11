e766ee976e8dcf79246eb49cb65460ad9c10ad25
lenaa00 <lena.lucedubas@ynov.com>
2025-03-11 15:18:37 +0100

    pres

--- projet/fonctions/launcher.go
+++ projet/fonctions/launcher.go
@@ -4,14 +4,9 @@ import (
 	"fmt"
 )
 
-// Lance l'application web
+
 func LaunchApp() {
 	fmt.Println("Lancement de l'application web")
 	Server(8080)
-	// err := IsWordPressSite("https://www.starwarsblog.net")
-	// if err != nil {
-	// 	fmt.Println("Erreur lors de la vérification du site WordPress: ", err)
-	// } else {
-	// 	fmt.Println("Le site EST un site WordPress")
-	// }
+	
 }
--- projet/fonctions/worldpressTester.go
+++ projet/fonctions/worldpressTester.go
@@ -7,20 +7,6 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-// func WordPressPageChecker(url string) bool {
-// 	if len(os.Args) > 1 {
-// 		url = os.Args[1]
-// 	} else {
-// 		fmt.Print("Entrez l'URL du site WordPress: ")
-// 		fmt.Scanln(&url)
-// 	}
-
-// 	if isWordPressSite(url) {
-// 		fmt.Printf("Le site %s est un site WordPress.\n", url)
-// 	} else {
-// 		fmt.Printf("Le site %s n'est pas un site WordPress.\n", url)
-// 	}
-// }
 
 func IsWordPressSite(url string) error {
 	resp, err := http.Get(url)
@@ -34,7 +20,6 @@ func IsWordPressSite(url string) error {
 		return errors.New("erreur lors de l'analyse du document")
 	}
 
-	// Vérifie la présence de balises spécifiques à WordPress
 	if doc.Find("meta[name='generator'][content^='WordPress']").Length() > 0 {
 		return nil
 	}
